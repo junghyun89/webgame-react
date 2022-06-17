9c71c1b766bce77c75f072a64580b61c07b8a7b9
jung89 <104253822+junghyun89@users.noreply.github.com>
2022-06-17 21:36:41 +0900

    YAY

---
+++ 2.끝말잇기/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+module.exports = {
+  name: 'wordrelay-setting',
+  mode: 'development', // 실서비스: production
+  devtool: 'eval',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  entry: {
+    app: ['./client'],
+  }, // 입력
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: {
+                  browsers: ['> 5% in KR'], // browserslist
+                },
+                debug: true,
+              },
+            ],
+            '@babel/preset-react',
+          ],
+          plugins: ['react-refresh/babel'],
+        },
+      },
+    ],
+  },
+  plugins: [new RefreshWebpackPlugin()],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'app.js',
+    publicPath: '/dist',
+  }, // 출력
+  devServer: {
+    devMiddleware: { publicPath: '/dist' },
+    static: { directory: path.resolve(__dirname) },
+    hot: true,
+  },
+};
