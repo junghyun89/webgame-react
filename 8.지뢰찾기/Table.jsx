5104897b1ad17081abf7b9350fc7f7b10c85cbd9
jung89 <104253822+junghyun89@users.noreply.github.com>
2022-06-22 23:19:28 +0900

    YAY

---
+++ 8.지뢰찾기/Table.jsx
@@ -0,0 +1,15 @@
+import React, { useContext, memo } from 'react';
+import { TableContext } from './MineSearch';
+import Tr from './Tr';
+
+const Table = memo(() => {
+  const { tableData } = useContext(TableContext);
+  return (
+    <table>
+      {Array(tableData.length).fill().map((tr, i) => <Tr rowIndex={i} />)}
+    </table>
+  )
+});
+Table.displayName = 'Table';
+
+export default Table;
